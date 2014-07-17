727244b42472a6cefd7b997657550efe852b0189
Misbah Khan <mk.misbah.khan@gmail.com>
2014-07-17 11:49:02 +0000

    began implementing instagram api through instagram-node

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -12,7 +11,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3030);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -27,9 +26,38 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+var api = require('instagram-node').instagram();
+
+api.use({ client_id: '992cfa2cf00e43838ff399ff394019f7',
+      client_secret: '7ca2d577e4144dda82a4acc157870b7f' });
+
+
+var redirect_uri = 'http://www.infinicoisn.com/handleauth';
+
+exports.authorize_user = function(req, res) {
+    res.redirect(api.get_authorization_url(rediret_url, { scope: ['likes'], state: 'a state' }));
+};
+
+exports.handleauth = function(req, res) {
+    api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+        if (err) {
+            console.log(err.body);
+            res.send("Didn't work"); 
+        } else {
+            console.log("token: "+result.access_token);
+            res.send("Worked"); 
+        }
+    });
+}
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/authorize_user', exports.authorize_user);
+app.get('/handleauth', exports.handleauth);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
